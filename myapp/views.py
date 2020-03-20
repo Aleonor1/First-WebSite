d8f72832cf1e68f1cc19bb4e41d24a8fbad1997c
Aleonor1 <adrianaleodor@gmail.com>
2020-03-20 20:26:25 +0200

    search bar, button and functionality added

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
+import requests
+from requests.compat import quote_plus
+from . import models
 # Create your views here.
 
+BASE_CRAIGLIST_URL ='https://losangeles.craigslist.org/search/sss?query={}'
 
 def homeView(request):
     return render(request,  'base.html')
 
 def newSearch(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    soup_title = soup.find_all('a',{'class': 'result-title'})
+    print(soup)
     stuffForFronted={
         'search':search
     }
